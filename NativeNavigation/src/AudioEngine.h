49baff28ec013a4b953000b511966b2d0d9f6f0e
kcoul <kieran.coulter@gmail.com>
2023-06-28 02:01:01 -0700

    Cleanup

--- NativeNavigation/src/AudioEngine.h
+++ NativeNavigation/src/AudioEngine.h
@@ -21,17 +21,16 @@ public:
     //==============================================================================
     void setRoomSize(float roomSize);    // roomSize goes from 0 to 1
     void setLowpassCutoff(float cutoff); // cutoff goes from 0 to 1 (1 being nyquist frequency)
-    // TODO add more DSP methods
 
     //==============================================================================
     // You can only have the waveform component be in single parent view
 
-    // returns a point to the native handle of the component: only needed on windows
+    // returns a pointer to the native handle of the component: only needed on windows
     void* addWaveformComponentToNativeParentView (void* nativeView);
     void removeWaveformComponentFromNativeParentView();
     void setWaveformComponentSize (int width, int height);  // Needed for iOS
 
-    void setPlaybackFinishedCallback (std::function<void()>); // 2
+    void setPlaybackFinishedCallback (std::function<void()>);
 
     //==============================================================================
     AudioEngine(const AudioEngine&) = delete;
